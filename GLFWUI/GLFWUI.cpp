f2f5398edfb79b1a379d65dc899aaac55b900bc8
Ollie Tedder <otlg123@gmail.com>
2019-02-09 17:13:03 -0800

    Basic UI working and plugged in to main program

--- GLFWUI/GLFWUI.cpp
+++ GLFWUI/GLFWUI.cpp
@@ -2,9 +2,8 @@
 //
 
 #include "pch.h"
-
-#include "GLFWUI.h"
 #include "glfw3.h"
+#include "GLFWUI.h"
 #include "glfw3native.h"
 #include <iostream>
 
@@ -23,33 +22,27 @@ bool GLFWUI::setupUI(const UIOptions &options)
 {
     m_options = options;
 
-    GLFWwindow* window;
-
-    if (!glfwInit())
-    {
-        return false;
-    }
-
-    int screenCount;
-    GLFWmonitor** monitors = glfwGetMonitors(&screenCount);
-
-    int screenIndex = screenCount - 1;
-    GLFWmonitor *screen = monitors[screenIndex];
-
-    window = glfwCreateWindow(m_options.windowWidth, m_options.windowHeight, "Hello World", screen, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return false;
-    }
-
-    glfwMakeContextCurrent(window);
-    glfwPollEvents();
-
     m_killRenderThread = false;
-    m_renderThread = std::make_unique<std::future<void>>(std::async([&]()
+    m_renderThread = std::make_unique<std::future<bool>>(std::async([&]()
     {
+        if (!glfwInit())
+        {
+            return false;
+        }
+
+        m_window = glfwCreateWindow(m_options.windowWidth, m_options.windowHeight, "Hello World", nullptr, nullptr);
+        if (!m_window)
+        {
+            glfwTerminate();
+            return false;
+        }
+
+        glfwMakeContextCurrent(m_window);
+
         renderLoop();
+
+        glfwTerminate();
+        return true;
     }));
 
     m_setup = true;
@@ -65,8 +58,6 @@ bool GLFWUI::quitUI()
         {
             m_renderThread->wait();
         }
-
-        glfwTerminate();
     }
     return true;
 }
@@ -77,12 +68,41 @@ bool GLFWUI::updateBoard(const boost::numeric::ublas::matrix<int> &board)
     return true;
 }
 
+void GLFWUI::drawBoard()
+{
+    const int height = m_board->size1();
+    const int width = m_board->size2();
+
+
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            // draw stuff
+        }
+    }
+}
+
 void GLFWUI::renderLoop()
 {
     while (!m_killRenderThread)
     {
-        //const matrix<int> &board = m_board->board();
+        glClearColor(0.f, 0.f, 0.f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        if (m_board)
+        {
+            drawBoard();
+        }
 
         glfwPollEvents();
+
+        m_killRenderThread = glfwWindowShouldClose(m_window);
+
+        glfwSwapBuffers(m_window);
+
+        glFinish();
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
 }
