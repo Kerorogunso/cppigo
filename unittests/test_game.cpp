8e42ada65bb02b2721504903c28e51345e2ebf29
Kerorogunso <albertswchung@gmail.com>
2019-02-11 05:37:51 +0000

    Doc strings for unit test board setups.

--- unittests/test_game.cpp
+++ unittests/test_game.cpp
@@ -2,12 +2,24 @@
 #include "cppigo_lib/GoGame.h"
 #include "cppigo_lib/Board.h"
 
+TEST(GoGame, MakeMove)
+{
+    GoGame testGame;
+    GobanError code = testGame.makeMove(1, 1);
+    EXPECT_EQ(code, GobanError::kSuccess);
+}
+
 TEST(GoGame, SelfAtari)
 {
     Goban testBoard(9);
     Player black("Player 1", kBlack);
     Player white("Player 2", kWhite);
-
+    
+    // Illegal white position on centre
+    //   0 1 2
+    // 0   B
+    // 1 B . B
+    // 2   B
     testBoard.placeStone(BLACK, 1, 0);
     testBoard.placeStone(BLACK, 0, 1);
     testBoard.placeStone(BLACK, 1, 2);
@@ -17,7 +29,11 @@ TEST(GoGame, SelfAtari)
     testGame.currentPlayer = testGame.white;
     GobanError selfAtariCheck = testGame.play(1, 1);
     EXPECT_EQ(selfAtariCheck, GobanError::kSelfAtari);
-
+    
+    // Illegal black position on corner
+    //   7 8
+    // 7   W
+    // 8 W . 
     testBoard.placeStone(WHITE, 7, 8);
     testBoard.placeStone(WHITE, 8, 7);
     GoGame testGame2(testBoard, black, white);
@@ -30,7 +46,12 @@ TEST(GoGame, KoRule)
     Goban testBoard(9);
     Player black("Player 1", kBlack);
     Player white("Player 2", kWhite);
-
+    
+    // Board setup
+    //   0 1 2 3
+    // 0   B W 
+    // 1 B . B W
+    // 2   B W 
     testBoard.placeStone(BLACK, 1, 0);
     testBoard.placeStone(BLACK, 0, 1);
     testBoard.placeStone(BLACK, 1, 2);
@@ -43,6 +64,12 @@ TEST(GoGame, KoRule)
     GoGame testGame(testBoard, black, white);
     testGame.currentPlayer = testGame.white;
     testGame.play(1, 1);
+    
+    // Replay on captured position is illegal as it makes a previous board state
+    //   0 1 2 3
+    // 0   B W 
+    // 1 B W . W
+    // 2   B W 
     GobanError invalidKo = testGame.play(1, 2);
     EXPECT_EQ(invalidKo, GobanError::kKoRule);
     
@@ -53,17 +80,22 @@ TEST(GoGame, CaptureCentre)
     Goban testBoard(9);
     Player black("Player 1", kBlack);
     Player white("Player 2", kWhite);
-
+    
+    // Black plays below white and captures successfully 
+    //   0 1 2
+    // 0   B
+    // 1 B W B
+    // 2   .
     testBoard.placeStone(BLACK, 1, 0);
     testBoard.placeStone(BLACK, 0, 1);
     testBoard.placeStone(BLACK, 1, 2);
-    testBoard.placeStone(BLACK, 2, 1);
+    testBoard.placeStone(WHITE, 1, 1);
 
     GoGame testGame(testBoard, black, white);
     testGame.currentPlayer = testGame.white;
 
-    GobanError atari = testGame.play(1, 1);
-    EXPECT_EQ(atari, GobanError::kSelfAtari);
+    GobanError capture = testGame.play(2, 1);
+    EXPECT_EQ(capture, GobanError::kSuccess);
     
 }
 
@@ -72,12 +104,17 @@ TEST(GoGame, CaptureCorner)
     Goban testBoard(13);
     Player black("Player 1", kBlack);
     Player white("Player 2", kWhite);
-
+    
+    // Black plays left of white in bottom corner and captures
+    //    11 12
+    // 11     B
+    // 12  .  W
     testBoard.placeStone(BLACK, 11, 12);
     testBoard.placeStone(WHITE, 12, 12);
     GoGame testGame(testBoard, black, white);
     GobanError captureCorner = testGame.play(12, 11);
     EXPECT_EQ(testGame.goban.board()(12, 12), EMPTY);
+    EXPECT_EQ(captureCorner, GobanError::kSuccess);
 }
 
 TEST(GoGame, CaptureSide)
@@ -86,6 +123,12 @@ TEST(GoGame, CaptureSide)
     Player black("Player 1", kBlack);
     Player white("Player 2", kWhite);
     
+    // Board setup on right hand side
+    //   17 18
+    // 5     W  
+    // 6  W  B
+    // 7  W  B
+    // 8     .
     testBoard.placeStone(BLACK, 5, 18);
     testBoard.placeStone(BLACK, 6, 18);
     testBoard.placeStone(WHITE, 5, 17);
@@ -96,8 +139,10 @@ TEST(GoGame, CaptureSide)
     testGame.currentPlayer = testGame.white;
 
     // Play capturing white stone
-    testGame.play(7, 18);
+    GobanError capture = testGame.play(7, 18);
 
     EXPECT_EQ(testGame.goban.board()(5, 18), EMPTY);
     EXPECT_EQ(testGame.goban.board()(6, 18), EMPTY);
+    EXPECT_EQ(capture, GobanError::kSuccess);
 }
+
